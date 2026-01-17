bdc857e8c4f4621796bbe2fb81662a57b37c3cae
ashirupraharshana <ashirupraharshana@gmail.com>
2026-01-17 16:59:44 +0530

    first commit

---
+++ backend/src/main/java/com/amanda/cinema/AmandaCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.amanda.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmandaCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmandaCinemaApplication.class, args);
+	}
+
+}
